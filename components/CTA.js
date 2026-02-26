d3991a0f524c9529ea778d35f2640413462c034c
Franklin Dambra <franklindambra@gmail.com>
2026-02-26 13:17:17 -0600

    cta link update

--- components/CTA.js
+++ components/CTA.js
@@ -7,7 +7,7 @@ import { trackEvent } from "fathom-client";
 export default function CTA({ resetMenus = () => {} }) {
   return (
     <Link
-      href="/schedule-a-consultation"
+      href="/subscribe"
       className="cta inline-block px-4 py-2 text-white text-center rounded-sm focus:outline-none bg-vf-green font-bold ease-in-out duration-200 hover:scale-[104%] shadow-2xl"
       onClick={() => {
         resetMenus();
